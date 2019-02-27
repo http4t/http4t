dd7784b7f2f8efadc1389874aad5064f51af6984
Daniel Worthington-Bodart <dan@bodar.com>
2019-02-27 21:48:53 +0000

    First draft of contract

---
+++ contract/src/contract.d.ts
@@ -0,0 +1,82 @@
+export type Method =
+    'GET'
+    | 'POST'
+    | 'PUT'
+    | 'PATCH'
+    | 'DELETE'
+    | 'HEAD'
+    | 'OPTIONS'
+    | 'TRACE'
+    | 'CONNECT'
+    | 'UPGRADE'
+    | string;
+
+export type Header =
+    'Accept'
+    | 'Accept-Charset'
+    | 'Accept-Encoding'
+    | 'Accept-Language'
+    | 'Authorization'
+    | 'Cache-Control'
+    | 'Content-Encoding'
+    | 'Content-Language'
+    | 'Content-Length'
+    | 'Content-Location'
+    | 'Content-Type'
+    | 'Content-MD5'
+    | 'Date'
+    | 'ETag'
+    | 'Expires'
+    | 'Host'
+    | 'If-Match'
+    | 'If-Modified-Since'
+    | 'If-None-Match'
+    | 'If-Unmodified-Since'
+    | 'Last-Modified'
+    | 'Location'
+    | 'User-Agent'
+    | 'Vary'
+    | 'WWW-Authenticate'
+    | 'Cookie'
+    | 'Set-Cookie'
+    | 'X-Forwarded-For'
+    | 'X-Forwarded-Proto'
+    | 'X-Forwarded-Host'
+    | 'X-Frame-Options'
+    | 'X-CorrelationID'
+    | 'Transfer-Encoding'
+    | 'Access-Control-Allow-Origin'
+    | string ;
+
+
+type Data = string | ArrayBuffer;
+
+type Body = Data | Promise<Data> | AsyncIterator<Data>;
+
+export interface Uri {
+    scheme: string | undefined;
+    authority: string | undefined;
+    path: string;
+    query: string | undefined;
+    fragment: string | undefined;
+}
+
+export interface Message {
+    readonly headers: Header[],
+    readonly body?: Body
+}
+
+export interface Request extends Message {
+    readonly method: Method
+    readonly uri: string | Uri,
+    readonly version?: string,
+}
+
+export interface Response extends Message {
+    readonly status: number,
+}
+
+export interface Handler {
+    handle(request: Request): Promise<Response>;
+}
+
