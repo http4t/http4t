b60cd09f232b73279c6dd4cc410a30bf597f79af
Matt Savage <matthew.savage@gmail.com>
2019-03-02 10:10:24 +0000

    Minor: stop distracting me with ids warnings

--- contract/src/handlers/bin.ts
+++ contract/src/handlers/bin.ts
@@ -10,15 +10,15 @@ export class BinHandler implements HttpHandler {
 
     if (method === 'GET' && path === '/stream-bytes') return this.streamBytes(10);//TODO: read out of querystring
     if (method === 'GET') return ok();
-    if (method === 'POST') return this.echo(request);
-    if (method === 'PUT') return this.echo(request);
-    if (method === 'PATCH') return this.echo(request);
-    if (method === 'DELETE') return this.echo(request);
+    if (method === 'POST') return BinHandler.echo(request);
+    if (method === 'PUT') return BinHandler.echo(request);
+    if (method === 'PATCH') return BinHandler.echo(request);
+    if (method === 'DELETE') return BinHandler.echo(request);
 
     return notFound();
   }
 
-  async echo({uri, headers, body}: HttpRequest): Promise<HttpResponse> {
+  static async echo({uri, headers, body}: HttpRequest): Promise<HttpResponse> {
     // TODO convert headers to object instead of array
     const jsonedHeaders = headers.reduce((acc: any, [n, v]) => {
       acc[n] = v;
