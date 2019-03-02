90197fa387c22c9f8d9fe0005d114eec5ed2d5bc
TomShacham <shachamtom@gmail.com>
2019-03-02 18:00:05 +0000

    reversible

---
+++ contract/src/UriTemplate.ts
@@ -0,0 +1,74 @@
+import { Uri } from "./uri";
+
+export type Captures = { [name: string]: string }
+
+export class UriTemplate {
+  private pathTemplate: string;
+  private queryTemplate: string;
+
+  constructor(private template: string) {
+    const [pathTemplate, queryTemplate] = this.template
+      .replace(/\/\?/, '?').replace(/\/$/, '') // remove trailing slash
+      .replace('{?', '?{').split('?'); // swap {? cos conforming to RFC is annoying for implementation
+
+    this.pathTemplate = pathTemplate;
+    this.queryTemplate = queryTemplate;
+  }
+
+  static of(template: string) {
+    return new UriTemplate(template)
+  }
+
+  matches(against: string): boolean {
+    const uri = Uri.parse(against);
+    return this.matchesPath(uri.path) && this.matchesQuery(uri.query);
+  }
+
+  extract(from: string): Captures {
+    const uri = Uri.parse(from);
+    return {
+      ...this.extractPathCaptures(uri.path),
+      ...this.extractQueryCaptures(uri.query)
+    };
+  }
+
+  uriFrom(captures: Captures): string {
+    return Object.keys(captures).reduce((rebuilt: string, capture: string) => {
+      return rebuilt
+        .replace(`{${capture}}`, captures[capture])
+        .replace(`{?${capture}`, `?${capture}=${captures[capture]}`) // start query
+        .replace(new RegExp(`,${capture}}?`), `&${capture}=${captures[capture]}`) // middle or end query
+    }, this.template).replace(/[{}]/g, '');
+  }
+
+  private extractQueryCaptures(query: string | undefined): Captures {
+    if (!query) return {};
+    return this.queryTemplate
+      .replace(/[{}]/g, '')
+      .split(',')
+      .reduce((captures: Captures, queryParameter: string) => {
+        const regExpMatchArray = query.match(new RegExp(`${queryParameter}=([^&]+)`));
+        captures[queryParameter] = regExpMatchArray![1];
+        return captures;
+      }, {});
+  }
+
+  private extractPathCaptures(path: string): Captures {
+    const pathParamVariableNames = (this.pathTemplate.match(/{([^}]+)}/g) || []).map(name => name.replace(/[{}]/g, ''));
+    const values = new RegExp(this.pathTemplate.replace(/{([^}]+)}/g, '(.+)'), 'g').exec(path);
+    return pathParamVariableNames.reduce((captures: Captures, param, index) => {
+      captures[param] = values![index + 1];
+      return captures;
+    }, {});
+  }
+
+  private matchesPath(path: string): boolean {
+    return new RegExp(this.pathTemplate.replace(/[{}]/g, '')).exec(path) !== null;
+  }
+
+  private matchesQuery(against: string | undefined): boolean {
+    if (!against || !this.queryTemplate) return true;
+    const captureGroup = new RegExp('\{([^\\}]+)\}').exec(this.queryTemplate);
+    return captureGroup![1].split(',').every(match => against.includes(match))
+  }
+}
--- contract/test/UriTemplate.test.ts
+++ contract/test/UriTemplate.test.ts
@@ -1,101 +1,54 @@
 import { expect } from "chai";
-import { Uri } from "../src/uri";
-
-
-export type Captures = { [name: string]: string }
-
-export class UriTemplate {
-  private adjustedTemplate: string;
-
-  constructor(private template: string) {
-    this.adjustedTemplate = this.template.replace('{?', '?{')
-  }
-
-  static of(template: string) {
-    return new UriTemplate(template)
-  }
-
-  matches(against: string): boolean {
-    const uri = Uri.parse(against);
-    return this.matchesPath(uri.path) && this.matchesQuery(uri.query);
-  }
-
-  extract(from: string): Captures {
-    const uri = Uri.parse(from);
-    const pathCaptures = this.extractPathCaptures(uri.path);
-    const queryCaptures = this.extractQueryCaptures(uri.query);
-    return {
-      ...pathCaptures,
-      ...queryCaptures
-    };
-  }
-
-  private extractQueryCaptures(query: string | undefined): Captures {
-    if (!query) return {};
-    return this.adjustedTemplate.split('?')[1]
-      .replace(/[{}]/g, '')
-      .split(',')
-      .reduce((captures: Captures, queryParameter) => {
-        const regExpMatchArray = query.match(new RegExp(`${queryParameter}=([^&]+)`));
-        captures[queryParameter] = regExpMatchArray![1];
-        return captures;
-      }, {});
-  }
-
-  private extractPathCaptures(path: string): Captures {
-    const pathTemplate = this.adjustedTemplate.split('?')[0];
-    const pathParamVariableNames = (pathTemplate.match(/{([^}]+)}/g) || []).map(name => name.replace(/[{}]/g, ''));
-    const values = new RegExp(pathTemplate.replace(/{([^}]+)}/g, '([^\/]+)'), 'g').exec(path);
-    return pathParamVariableNames.reduce((captures: Captures, param, index) => {
-      captures[param] = values![index + 1];
-      return captures;
-    }, {});
-  }
-
-  private matchesPath(against: string): boolean {
-    const pathTemplate = this.adjustedTemplate.split('?')[0];
-    return new RegExp(pathTemplate.replace('{', '(').replace('}', ')'))
-      .exec(against) !== null;
-  }
-
-  private matchesQuery(against: string | undefined): boolean {
-    if (!against) return true;
-    const queryTemplate = '{' + this.template.split('?')[1];
-    if (queryTemplate) {
-      const captureGroup = new RegExp('\{([^\\}]+)\}').exec(queryTemplate);
-      if (captureGroup) {
-        return captureGroup[1].split(',').every(match => against.includes(match))
-      }
-    }
-    return true;
-  }
-}
+import { UriTemplate } from "../src/UriTemplate";
 
 describe('UriTemplate', () => {
-  it('matches paths or not', () => {
-    const uriTemplate = UriTemplate.of('/part/{capture}/part');
+  it('matches uris or not', () => {
+    const uriTemplate = UriTemplate.of('/part/{capture}/part/?query1=value1');
+
+    expect(uriTemplate.matches('/doesnt/match')).eq(false);
+    expect(uriTemplate.matches('/part/capture/part')).eq(true);
+  });
+
+  it('ignores trailing slashes', () => {
+    const uriTemplate = UriTemplate.of('/part/{capture}/part/');
 
     expect(uriTemplate.matches('/doesnt/match')).eq(false);
     expect(uriTemplate.matches('/part/capture/part')).eq(true);
     expect(uriTemplate.matches('/part/capture/part/')).eq(true);
+    expect(uriTemplate.matches('/part/capture/part?query=value')).eq(true);
+    expect(uriTemplate.matches('/part/capture/part/?query=value')).eq(true);
   });
 
   it('matches query or not', () => {
-    const uriTemplate = UriTemplate.of('/part/{capture}/part{?query1,query2}');
+    const uriTemplate = UriTemplate.of('/part/{capture}/part/{?query1,query2}');
 
     expect(uriTemplate.matches('/part/capture/part?query1=value1')).eq(false);
-    expect(uriTemplate.matches('/part/capture/part?query1=value1&query2=value2')).eq(true);
+    expect(uriTemplate.matches('/part/capture/part/?query1=value1&query2=value2')).eq(true);
   });
 
-  it('extract captures', () => {
-    const uriTemplate = UriTemplate.of('/part/{capture1}/{capture2}/part{?query1,query2}');
+  it('extracts captures', () => {
+    const uriTemplate = UriTemplate.of('/part/{capture1}/{capture2}/part/{?query1,query2}');
 
-    expect(uriTemplate.extract('/part/one/two/part?query1=value1&query2=value2&query3=value3')).eql({
+    expect(uriTemplate.extract('/part/one/two/part/?query1=value1&query2=value2&query3=value3')).eql({
       capture1: 'one',
       capture2: 'two',
       query1: 'value1',
       query2: 'value2'
     });
+  });
+
+  it('is reversible', () => {
+    const uriTemplate1 = UriTemplate.of('/part/{capture1}/part{?query1}');
+    const captures1 = uriTemplate1.extract('/part/one/two/part?query1=value1');
+    expect(uriTemplate1.uriFrom(captures1)).eq('/part/one/two/part?query1=value1');
+
+    const uriTemplate2 = UriTemplate.of('/part/{capture1}/{capture2}/part{?query1,query2}');
+    const captures2 = uriTemplate2.extract('/part/one/two/part?query1=value1&query2=value2');
+    expect(uriTemplate2.uriFrom(captures2)).eq('/part/one/two/part?query1=value1&query2=value2');
+
+    const uriTemplate3 = UriTemplate.of('/part/{capture1}/{capture2}/part{?query1,query2,query3}');
+    const captures3 = uriTemplate3.extract('/part/one/two/part?query1=value1&query2=value2&query3=value3');
+    expect(uriTemplate3.uriFrom(captures3)).eq('/part/one/two/part?query1=value1&query2=value2&query3=value3');
   });
 
 });
