92cae2721649c16efb73918a4bd72879732d2080
TomShacham <shachamtom@gmail.com>
2019-04-27 14:35:22 +0100

    bump version script

---
+++ BumpVersion.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs';
+
+type Dependencies = { [dep: string]: string };
+
+function getPackageJson(path: string) {
+  return JSON.parse(fs.readFileSync(path).toString('utf-8'));
+}
+
+(async function bumpVersion() {
+  const write = process.argv.slice(2)[0] === 'write';
+
+  const circleBuildNumber = process.env.CIRCLE_BUILD_NUM;
+  const tldPackageJson = getPackageJson('.');
+  const localVersionNumber = tldPackageJson.version;
+  const latestVersion = circleBuildNumber || localVersionNumber;
+  tldPackageJson.version = latestVersion;
+
+  const packages = fs.readdirSync('.').filter(it => it.match(/http4t\-/));
+  packages.forEach(pkg => {
+    ['', 'src/', 'test/'].forEach(packageJsonDir => {
+      const path = `${pkg}/${packageJsonDir}package.json`;
+      const packageJson = getPackageJson(path);
+      packageJson.version = latestVersion;
+      const dependencies: Dependencies = packageJson.dependencies;
+
+      if (dependencies) {
+        Object.keys(dependencies).forEach((name) => {
+          if (name.match(/@http4t/)) {
+            packageJson.dependencies[name] = latestVersion;
+          }
+        })
+      }
+
+      if (write) {
+        fs.writeFileSync(path, JSON.stringify(packageJson))
+      }
+    })
+
+  })
+})();
