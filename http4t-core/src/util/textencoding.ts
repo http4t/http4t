bfa929d02fb289001c3b9904300bb249a025eb81
Matt Savage <matthew.savage@gmail.com>
2019-08-16 20:17:01 +0100

    Fixing build

--- http4t-core/src/util/textencoding.ts
+++ http4t-core/src/util/textencoding.ts
@@ -1,13 +1,13 @@
 export function textDecoder(): TextDecoder {
   if (typeof TextDecoder === 'function')
     return new TextDecoder('utf-8');
-  const util = require('');
+  const util = require('util');
   return new util.TextDecoder('utf-8')
 }
 
 export function textEncoder(): TextEncoder {
   if (typeof TextEncoder === 'function')
     return new TextEncoder();
-  const util = require('');
+  const util = require('util');
   return new util.TextEncoder()
 }
