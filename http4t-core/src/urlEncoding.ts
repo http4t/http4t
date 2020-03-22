e97417cf8375971e0239dff05a764c0e83f4f879
Matt Savage <matthew.savage@gmail.com>
2020-03-22 15:45:44 +0000

    #5 url encoding

---
+++ http4t-core/src/urlEncoding.ts
@@ -0,0 +1,35 @@
+export type DecodedPair = [string, string | null];
+
+export function decode(value: string): string {
+  return decodeURIComponent(value.replace('+', ' '));
+}
+
+export function encode(value: string): string {
+  return encodeURIComponent(value).replace('%20', '+');
+}
+
+export function parsePair(pair: string): DecodedPair {
+  const [name, value] = pair.split('=');
+  const decoded = pair.indexOf('=') < 0 ? null : decode(value);
+  return [decode(name), decoded]
+}
+
+export function parse(value: string): DecodedPair[] {
+  return value
+    .split('&')
+    .map(p => parsePair(p));
+}
+
+export function unparsePair([name, value]: DecodedPair): string {
+  const encodedValue = value === null
+    ? ""
+    : `=${encode(value)}`;
+
+  return `${encode(name)}${encodedValue}`
+}
+
+export function unparse(values: DecodedPair[]): string {
+  return values
+    .map(unparsePair)
+    .join('&');
+}
---
+++ http4t-core/test/urlEncoding.test.ts
@@ -0,0 +1,39 @@
+import {expect} from 'chai';
+import {DecodedPair, parse, unparse} from "../src/urlEncoding";
+
+function checkParsing(value: string, expected: DecodedPair[]) {
+  expect(parse(value)).deep.eq(expected);
+  expect(unparse(expected)).eq(value);
+}
+
+describe('parse() and unparse()', () => {
+  it('Supports simple names and values', async () => {
+    checkParsing(
+      'name=value',
+      [['name', 'value']]);
+  });
+  it('Supports multiple pairs', async () => {
+    checkParsing(
+      'name=value&name=value&other-name=other-value&empty=&null',
+      [['name', 'value'], ['name', 'value'], ['other-name', 'other-value'], ['empty', ''], ['null', null]]);
+  });
+  it('Supports percent encoding', async () => {
+    checkParsing(
+      'name%C3%BC%40=value%C3%BC%40',
+      [['nameü@', 'valueü@']]);
+  });
+  it('Supports spaces', async () => {
+    checkParsing(
+      'the+name=the+value',
+      [['the name', 'the value']]);
+  });
+  it('Supports empty value', async () => {
+    checkParsing(
+      'name=',
+      [['name', '']])
+  });
+  it('Supports no value', async () => {
+    checkParsing('name',
+      [['name', null]])
+  });
+});
