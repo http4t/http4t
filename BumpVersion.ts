b5886dd3192f6a3b27b375f35a65301972f341e5
Matt Savage <matthew.savage@gmail.com>
2020-06-06 16:48:45 +0100

    Removing unused code; getting log output to try to fix circleci

--- BumpVersion.ts
+++ BumpVersion.ts
@@ -15,12 +15,14 @@ function getPackageJson(path: string) {
   const majorAndMinorVersion = localVersionNumber.slice(0, localVersionNumber.lastIndexOf('.'));
 
   const latestVersion = circleBuildNumber ? `${majorAndMinorVersion}.${circleBuildNumber}` : localVersionNumber;
+  console.log(`CIRCLE_BUILD_NUM=${circleBuildNumber}`);
   tldPackageJson.version = latestVersion;
 
   const packages = fs.readdirSync('.').filter(it => it.match(/http4t\-/));
   packages.forEach(pkg => {
     ['', 'src/', 'test/'].forEach(packageJsonDir => {
       const path = `${pkg}/${packageJsonDir}package.json`;
+      console.log(`Bumping ${path} to ${latestVersion}`);
       const packageJson = getPackageJson(path);
       packageJson.version = latestVersion;
       const dependencies: Dependencies = packageJson.dependencies;
