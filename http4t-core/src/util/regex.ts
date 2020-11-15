9d5efdf8800701b87175579868c7dc3da2e383c2
Matt Savage <matthew.savage@gmail.com>
2020-11-15 13:18:51 +0000

    Stricter tsconfig.json

--- http4t-core/src/util/regex.ts
+++ http4t-core/src/util/regex.ts
@@ -15,14 +15,14 @@ export class Regex {
         const regex = new RegExp(this.pattern, 'g');
         let start = 0;
         let nonMatch = '';
-        let rest = '';
+        // let rest = '';
         let match;
         while (match = regex.exec(against)) {
             if (match) {
                 const end = against.indexOf(match[0]);
                 nonMatch = against.slice(start, end);
                 start = end + match[0].length;
-                rest = against.slice(start);
+                // rest = against.slice(start);
                 this.matched.push(match);
                 this.nonMatched.push(nonMatch);
 
