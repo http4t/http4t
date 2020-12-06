85a47d06238d1d2404bf2381397b67e9d9b69987
Matt Savage <matthew.savage@gmail.com>
2020-12-06 13:19:28 +0000

    Finally, a reliable clean build

--- README.md
+++ README.md
@@ -10,6 +10,125 @@ A modular web framework for Typescript
 
 Supports [RFC 2324](https://tools.ietf.org/html/rfc2324)
 
-## Project structure
+## Contributing
 
-We use yarn workspaces and Typescript composite projects. 
+### Build commands
+
+#### On first checkout
+
+```
+yarn install
+yarn run build
+```
+
+####To run all tests
+
+```
+yarn run test
+```
+
+#### To run a clean build
+
+```
+yarn run clean
+./delete_node_modules
+yarn install
+yarn run build
+```
+### Creating a new module
+
+Add new directory to root:
+
+```
+http4t-my-module
+   src
+      package.json
+      tsconfig.json
+   test
+      package.json
+      tsconfig.json
+```
+
+`http4t-my-module/src/package.json`:
+
+Note dependency on `@http4t/core`.
+
+```
+{
+  "name": "@http4t/my-module",
+  "version": "1.0.0",
+  "license": "Apache-2.0",
+  "type": "module",
+  "scripts": {
+    "build": "tsc --build"
+  },
+  "dependencies": {
+    "@http4t/core": "1.0.0"
+  }
+}
+```
+
+`http4t-my-module/src/tsconfig.json`:
+
+Note reference to `@http4t/core` source.
+
+```
+{
+  "extends": "../../tsconfig-base.json",
+  "references": [
+    {
+      "path": "../../http4t-core/src"
+    }
+  ]
+}
+```
+
+`http4t-my-module/test/package.json`:
+
+Note `name: "@http4t/my-module-test"`
+
+```
+{
+  "name": "@http4t/my-module-test",
+  "version": "1.0.0",
+  "license": "Apache-2.0",
+  "type": "module",
+  "scripts": {
+    "build": "tsc --build",
+    "test": "NODE_ENV=development mocha --require ts-node/register --colors --exit  '**/*.test.ts'",
+    "test:browser": "puppet"
+  },
+  "dependencies": {
+    "@http4t/core": "1.0.0",
+    "@http4t/mocha-puppeteer": "1.0.0",
+    "@http4t/my-module": "1.0.0"
+  },
+  "devDependencies": {
+    "@types/chai": "^4.1.7",
+    "@types/mocha": "^5.2.6",
+    "chai": "^4.1.7",
+    "mocha": "^7.0.1"
+  }
+}
+```
+
+`http4t-my-module/test/tsconfig.json`:
+
+Note reference to `@http4t/core` source.
+
+```
+{
+  "extends": "../../tsconfig-base.json",
+  "references": [
+    {
+      "path": "../src"
+    },
+    {
+      "path": "../../http4t-mocha-puppeteer/src"
+    },
+    {
+      "path": "../../http4t-core/src"
+    }
+  ]
+}
+```
--- build/build.ts
+++ build/build.ts
@@ -1,6 +1,21 @@
 import {spawn} from 'child_process';
 import {Package, packages, readPackage} from "./packages";
 
+/**
+ * Builds all modules
+ *
+ * Responsibilities:
+ *
+ * - Find all modules by `package.json` file
+ * - Figure out topological order of projects by dependency so we build dependencies first
+ * - For each module:
+ *   - yarn install
+ *   - Link mocha-puppeteer into the projects that need it, because installing it doesn't work
+ *   - Compile typescript
+ *   - yarn test
+ *   - yarn test:browser
+ */
+
 type Edges = { [node: string]: Set<string> };
 type Graph = {
     nodes: string[],
@@ -57,6 +72,11 @@ function topological(graph: Graph): string[] {
     return sorted;
 }
 
+/**
+ * Spawns a process
+ * pipes stderr/stdout
+ * Resolves or rejects promise on completion
+ */
 function spawnPromise(command: string, args: string[], cwd: string) {
     console.log(`${command} ${args.join(" ")}`);
     const test = spawn(command, args, {cwd});
@@ -75,16 +95,34 @@ function spawnPromise(command: string, args: string[], cwd: string) {
 
 const ignored = new Set(["@http4t/root", "@http4t/site"]);
 
+/**
+ * For some reason installing other yarn workspaces does not reliably
+ * create the `puppet` command in `node_modules/bin`, whereas linking
+ * works consistently.
+ */
+async function linkMochaPuppeteer(modules: Packages, install: boolean) {
+    const mochaPuppeteer = modules["@http4t/mocha-puppeteer"];
+    if (install) await spawnPromise("yarn", ["link"], mochaPuppeteer.path)
+}
+
 (async function build() {
+    const args = process.argv.slice(2);
+    const install = args[0] === 'install';
+
     const modules = packages(".")
-        .reduce((acc, path) => {
-            const pack = readPackage(path);
-            if (ignored.has(pack.name))
+        .reduce(
+            (acc, path) => {
+                const pack = readPackage(path);
+                if (ignored.has(pack.name))
+                    return acc;
+                const dir = path.replace(/\/package.json/, "");
+                acc[pack.name] = {path: dir, package: pack};
                 return acc;
-            const dir = path.replace(/\/package.json/, "");
-            acc[pack.name] = {path: dir, package: pack};
-            return acc;
-        }, {} as Packages);
+            },
+            {} as Packages);
+
+    if(install) await linkMochaPuppeteer(modules, install);
+
     const graph = buildGraph(modules);
 
     for (const name of topological(graph)) {
@@ -92,7 +130,17 @@ const ignored = new Set(["@http4t/root", "@http4t/site"]);
         console.log(name);
         console.log(module.path);
         const cwd = module.path;
-        await spawnPromise("yarn", ["install"], cwd);
+
+        if (install) {
+            await spawnPromise("yarn", ["install"], cwd);
+
+            // See linkMochaPuppeteer
+            if (module.package.devDependencies["@http4t/mocha-puppeteer"]
+                || module.package.dependencies["@http4t/mocha-puppeteer"]) {
+                await spawnPromise("yarn", ["link", "@http4t/mocha-puppeteer"], cwd);
+            }
+        }
+
         await spawnPromise("yarn", ["build"], cwd);
         if (cwd.endsWith("/test")) {
             await spawnPromise("yarn", ["run", "test"], cwd);
