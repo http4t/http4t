1431c123d4f6bd5f550e276614a0f273b725efb7
Matt Savage <matthew.savage@gmail.com>
2020-12-10 13:49:42 +0000

    Fixing publishing in github actions

--- build/publish.ts
+++ build/publish.ts
@@ -15,13 +15,21 @@ import {spawnPromise} from "./util/processes";
     await spawnPromise("git", ["tag", tag]);
     await spawnPromise("git", ["push", "origin", tag]);
 
+    // Make sure everything builds before publishing, so we
+    // release either all modules or none to npm
+    for (const pack of Object.values(packages("packages"))) {
+        if (pack.path.endsWith("/test"))
+            continue;
+
+        await spawnPromise("yarn", ["run", "build"], pack.path);
+    }
+
     for (const pack of Object.values(packages("packages"))) {
         if (pack.path.endsWith("/test"))
             continue;
 
         console.log(`Publishing ${pack.package.name} ${pack.package.version}`);
 
-        await spawnPromise("yarn", ["run", "build"], pack.path);
         // Picks up NODE_AUTH_TOKEN set in github action
         await spawnPromise("npm", ["publish", '--access', 'public'], pack.path);
     }
