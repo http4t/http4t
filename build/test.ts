c770c668068179ba54d7020c3fc728fafaf67ea8
Matt Savage <matthew.savage@gmail.com>
2020-12-10 18:54:40 +0000

    Cleaning up after fighting npm

--- build/test.ts
+++ build/test.ts
@@ -1,24 +1,25 @@
-import {dependencyOrder} from "./util/graph";
 import {packages, Packages} from "./util/packages";
 import {spawnPromise} from "./util/processes";
 
 (async function test() {
-    const dirs = process.argv.slice(2);
+    const args = process.argv.slice(2);
+    const types = new Set(args.slice(0, 1)[0].split(","));
+    const dirs = args.slice(1);
 
     const modules = dirs.reduce(
         (packs, dir) => {
             return {...packs, ...packages("packages")};
         }, {} as Packages)
 
-    for (const name of dependencyOrder(modules)) {
-        const module = modules[name];
-        console.log(name);
+    for (const module of Object.values(modules)) {
+        console.log(module.package.name);
         console.log(module.path);
+
         const cwd = module.path;
 
         if (cwd.endsWith("/test")) {
-            await spawnPromise("yarn", ["run", "test"], cwd);
-            await spawnPromise("yarn", ["run", "test:browser"], cwd);
+            if (types.has("node")) await spawnPromise("yarn", ["run", "test"], cwd);
+            if (types.has("browser")) await spawnPromise("yarn", ["run", "test:browser"], cwd);
         }
     }
 })().then(_result => {
