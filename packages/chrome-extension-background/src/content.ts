5acc67032aa1c1302359f281e3a9796e5158b0b7
Matt Savage <matthew.savage@gmail.com>
2020-12-23 12:56:35 +0000

    Allow injection of HttpHandler in chrome extension content page

--- packages/chrome-extension-background/src/content.ts
+++ packages/chrome-extension-background/src/content.ts
@@ -1,19 +1,19 @@
 import {FetchHandler} from "@http4t/browser/fetch";
 import {bufferText} from "@http4t/core/bodies";
+import {HttpHandler} from "@http4t/core/contract";
 import {FetchMessage, isFetchMessage} from "./FetchViaBackgroundScript";
 
 /**
  * Run this in a content script that has permissions to run in tabs for the target host
  */
-export function startContentPageListener() {
+export function startContentPageListener(http: HttpHandler = new FetchHandler()) {
     chrome.runtime.onMessage.addListener((message: FetchMessage | any, sender, sendResponse) => {
         if (!isFetchMessage(message))
             return;
-        new FetchHandler({credentials: "same-origin"})
+        http
             .handle(message.request)
             .then(async response => {
                 const buffered = {...response, body: await bufferText(response.body)};
-                console.log("response", buffered)
                 sendResponse(buffered);
             })
             .catch(err => {
