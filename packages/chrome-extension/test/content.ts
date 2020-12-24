842c47c19e787165b3b82d15394adf0cf8141fcd
Matt Savage <matthew.savage@gmail.com>
2020-12-24 12:44:31 +0000

    Fixing chrome extension

--- packages/chrome-extension/test/content.ts
+++ packages/chrome-extension/test/content.ts
@@ -1,5 +1,5 @@
 import {FetchHandler} from "@http4t/browser/fetch";
-import {startContentPageListener} from "@http4t/chrome-extension/content";
+import {startContentScriptListener} from "@http4t/chrome-extension/content";
 import {LoggingHttpHandler} from "@http4t/core/LoggingHttpHandler";
 
-startContentPageListener(new LoggingHttpHandler(new FetchHandler()))
+startContentScriptListener(new LoggingHttpHandler(new FetchHandler()))
