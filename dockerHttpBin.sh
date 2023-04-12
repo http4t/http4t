38b4c2a14baed7bec611977241d601d5332d2060
Matthew Savage <matthew.savage@gmail.co.uk>
2023-04-12 11:06:58 +0100

    Fixing tests

--- dockerHttpBin.sh
+++ dockerHttpBin.sh
@@ -4,6 +4,11 @@ set -e
 
 CONTAINER_NAME=httpbin
 
+if [[ ! -z "${GITHUB_ACTION}" ]]; then
+  echo "Not starting httpbin- GITHUB_ACTION was set"
+  exit 0
+fi
+
 if [[ $(docker ps -a --filter="name=$CONTAINER_NAME" --filter "status=exited" | grep -w "$CONTAINER_NAME") ]]; then
   docker start httpbin
 elif [[ $(docker ps -a --filter="name=$CONTAINER_NAME" --filter "status=running" | grep -w "$CONTAINER_NAME") ]]; then
